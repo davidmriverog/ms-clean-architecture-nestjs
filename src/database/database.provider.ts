1cbf72e7665f80d495d9041e9707b4ff0bf2d298
David Rivero <davidmriverog@gmail.com>
2023-11-11 19:32:12 -0300

    database module

---
+++ src/database/database.provider.ts
@@ -0,0 +1,31 @@
+import { DataSource, getMetadataArgsStorage } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from './database.strategy';
+
+export const DatabaseProvider = [
+  {
+    provide: DataSource,
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        applicationName: 'Guayaba-API',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_DATABASE'),
+        entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+        synchronize: false,
+        logging: Boolean(configService.get('DB_LOGGING')),
+        namingStrategy: new SnakeNamingStrategy(),
+        useUTC: true,
+        poolSize: 10,
+      });
+
+      if (!dataSource.isInitialized) await dataSource.initialize();
+
+      return dataSource;
+    },
+    inject: [ConfigService],
+  },
+];
